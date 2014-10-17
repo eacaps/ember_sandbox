ae4fd95fbb686ebc2ff26f7e9780b9e0275b97c9
Eric Capito <eacaps@gmail.com>
2014-10-17 15:27:07 -0400

    corrected view with url params passing

--- addons/ol3/ember.ol3map.js
+++ addons/ol3/ember.ol3map.js
@@ -4,8 +4,7 @@ function getMapHeight() {
 }
 
 if('undefined' === typeof Ol3Map) {
-	Ol3Map = Ember.Application.create();
-	Ol3Map.IndexView = Ember.View.extend({});
+	Ol3Map = Ember.Namespace.create();
 }
 
 Ol3Map.Ol3MapController = Ember.Controller.extend({
@@ -26,25 +25,30 @@ Ol3Map.Ol3MapController = Ember.Controller.extend({
 })
 
 Ol3Map.Ol3MapView = Ember.View.extend({
+	lat:0,
+	lon:0,
+	zoom:0,
+	rotate:0,
+	_register: function() {
+		this.set('handle', this);
+	}.on('init'),
 	didInsertElement: function() {
-		var self = this;
+		var _this = this;
 		
 		var height = getMapHeight();
 		this.get('element').style['height'] = height + "px";
 		$(window).resize(function() {
-			self.get('element').style['height'] = getMapHeight() + "px";
-			self.get('element').style['width'] = $(window).width() + "px";
+			_this.get('element').style['height'] = getMapHeight() + "px";
+			_this.get('element').style['width'] = $(window).width() + "px";
 		})
 		if(this.get('element').style['width'] == '') {
-			self.get('element').style['width'] = $(window).width() + "px";
+			_this.get('element').style['width'] = $(window).width() + "px";
 		}
 		
-		var parentcontroller = this.controller.parentController;
-		
-		var mlon = parentcontroller.lon ? parentcontroller.lon : 0;
-		var mlat = parentcontroller.lon ? parentcontroller.lat : 0;
-		var mzoom = parentcontroller.lon ? parentcontroller.zoom : 0;
-		var mrotate = parentcontroller.lon ? parentcontroller.rotate : 0;
+		var mlon = this.get('lon');
+		var mlat = this.get('lat');
+		var mzoom = this.get('zoom');
+		var mrotate = this.get('rotate');
 		
 		var view = new ol.View({
 			center: [mlon,mlat],
@@ -76,6 +80,9 @@ Ol3Map.Ol3MapView = Ember.View.extend({
 			var view = map.getView();
 			var center = view.getCenter();
 			var zoom = view.getZoom();
+			_this.set('zoom', zoom);
+			_this.set('lat', center[1]);
+			_this.set('lon', center[0]);
 		})
 		
 		map.on('pointermove', function() {
@@ -84,9 +91,57 @@ Ol3Map.Ol3MapView = Ember.View.extend({
 		view.on('change:rotation', function(evt) {
 			var view = evt.target;
 			var rads = view.getRotation();
-			self.controller.parentController.set('rotate', rotation);
+			_this.set('rotate', rotation);
 		})
 		
-		this.controller.set('map', map);
-	}
+		this.set('map', map);
+	},
+	
+	actions: {
+		zoomToObject: function(object, type) {
+			if(type == "layer") {
+				var map = this.get('map');
+				var vectorsource = object.getSource();
+				var view = map.getView();
+				view.fitExtent(vectorsource.getExtent(), map.getSize());
+			} else if(tupe == "geom") {
+				var map = this.get('map');
+				var view = map.getView();
+				view.fitExtent(object.getExtent(), map.getSize());
+			}
+		}
+	},
+	
+	observeCenterChange: function(self, trigger) {
+		var vlat = this.get('lat');
+		var vlon = this.get('lon');
+		var vzoom = this.get('zoom');
+		var map = this.get('map');
+		
+		if(map) {
+			var view = map.getView();
+			var center = view.getCenter();
+			var center = view.getCenter();
+			var mlat = center[1];
+			var mlon = center[0];
+			var mzoom = view.getZoom();
+			if(mlat != vlat || mlon != vlon) {
+				view.setCenter([vlon, vlat]);
+			}
+			if(mzoom != vzoom) {
+				view.setZoom(vzoom);
+			}
+		}
+	}.observes('this.lat', 'this.lon', 'this.zoom'),
+	observeRotationChange: function(self, trigger) {
+		var rotate = eval(trigger);
+		var map = this.get('map');
+		if(map) {
+			var view = map.getView();
+			var currot = view.getRotation();
+			if(currot != rotate) {
+				view.setRotation(rotate);
+			}
+		}
+	}.observes('this.rotate')
 });
